b9f76f712468aac1fb189fa06e1ab8ca72bf9d95
Thiago van Dieten <thiago@tvdieten.com>
2025-06-09 09:57:27 +0200

    fix embarrassing typo in schema

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 
 CREATE TABLE users (
-    id UUID PRIMARY KEY
-    created_at TIMESTAMP NOT NULL
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT UNIQUE NOT NULL
 );
