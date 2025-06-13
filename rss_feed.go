86a1c22da12d629e7070f5760874b69f24df92be
Thiago van Dieten <thiago@tvdieten.com>
2025-06-13 20:34:29 +0200

    implement fetchFeed

---
+++ rss_feed.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"io"
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+
+	// I think I would like a HTTP client in the state
+	// but for now I will just create one here for it's currently the
+	// only function that requires one
+
+	client := &http.Client{}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return &RSSFeed{}, nil
+	}
+
+	req.Header.Add("User-Agent", "gator/0.0.1")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return &RSSFeed{}, err
+	}
+
+	feed := RSSFeed{}
+
+	xml.Unmarshal(data, &feed)
+
+	return &feed, nil
+}
