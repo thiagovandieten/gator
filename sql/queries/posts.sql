6c17dc229487adea38461092a0be0179e599a686
Thiago van Dieten <thiago@tvdieten.com>
2025-08-05 08:15:35 +0200

    add GetPostsByUser to posts queries

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -1,3 +1,10 @@
 -- Name: CreatePost :exec
 INSERT INTO posts(created_at, updated_at, title, url, description, published_at, feed_id)
-VALUES ($1, $2, $3, $4, $5, $6, $7);
+VALUES ($1, $2, $3, $4, $5, $6, $7);s
+
+-- Name: GetPostsByUser :many
+SELECT * 
+FROM posts
+WHERE user_id = $1
+ORDER BY created_at DESC
+LIMIT $2;
