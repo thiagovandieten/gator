ed3c8006bfbf84158a490ae0ddbaf0557c61c20c
Thiago van Dieten <thiago@tvdieten.com>
2025-08-07 19:48:51 +0200

    fix the query file to use proper sqlc formatting

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -1,10 +1,13 @@
--- Name: CreatePost :exec
+-- name: CreatePost :exec
 INSERT INTO posts(created_at, updated_at, title, url, description, published_at, feed_id)
 VALUES ($1, $2, $3, $4, $5, $6, $7);
 
--- Name: GetPostsByUser :many
+-- name: GetPostsByUser :many
+WITH user_feeds AS (
+    SELECT id FROM feeds WHERE user_id = $1
+)
 SELECT * 
 FROM posts
-WHERE user_id = $1
+WHERE feed_id IN (SELECT id FROM user_feeds)
 ORDER BY created_at DESC
 LIMIT $2;
