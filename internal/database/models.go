7276df0b3dffe39e0b0c5a9204d354c5f020ee0b
Thiago van Dieten <thiago@tvdieten.com>
2025-08-07 18:42:06 +0200

    fix embarrasing typo and generate models with sqlc

--- internal/database/models.go
+++ internal/database/models.go
@@ -29,6 +29,17 @@ type FeedFollow struct {
 	FeedID    int32
 }
 
+type Post struct {
+	ID          int32
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	Title       string
+	Url         string
+	Description sql.NullString
+	PublishedAt time.Time
+	FeedID      int32
+}
+
 type User struct {
 	ID        uuid.UUID
 	CreatedAt time.Time
--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -1,6 +1,6 @@
 -- Name: CreatePost :exec
 INSERT INTO posts(created_at, updated_at, title, url, description, published_at, feed_id)
-VALUES ($1, $2, $3, $4, $5, $6, $7);s
+VALUES ($1, $2, $3, $4, $5, $6, $7);
 
 -- Name: GetPostsByUser :many
 SELECT * 
